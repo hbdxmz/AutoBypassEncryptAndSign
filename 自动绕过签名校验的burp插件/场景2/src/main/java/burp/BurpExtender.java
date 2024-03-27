27cf3614a7030ee93863cbe550e5fa5f9e18b298
hbdxmz <94107024+hbdxmz@users.noreply.github.com>
2024-03-27 16:26:24 +0800

    1

--- 自动绕过签名校验的burp插件/场景2/src/main/java/burp/BurpExtender.java
+++ 自动绕过签名校验的burp插件/场景2/src/main/java/burp/BurpExtender.java
@@ -4,7 +4,6 @@ package burp;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 public class BurpExtender implements IBurpExtender,IHttpListener {
     private PrintWriter stdout;
@@ -77,58 +76,6 @@ public class BurpExtender implements IBurpExtender,IHttpListener {
             sig = MD5Util.getMD5Str(finalStr);
             stdout.println("sig: "+sig);
             messageInfo.setRequest(helpers.addParameter(req,helpers.buildParameter("sig",sig,IParameter.PARAM_URL)));
-        }
-
-
-        if(toolFlag == IBurpExtenderCallbacks.TOOL_REPEATER && messageIsRequest){
-            IRequestInfo requestInfo = helpers.analyzeRequest(messageInfo);
-
-            List<IParameter> parameters = requestInfo.getParameters().;
-
-            for(IParameter parameter : parameters){
-                if(parameter.getType() == IParameter.PARAM_BODY){
-                    //移除原来的api_sign
-                    if("sig".equals(parameter.getName())){
-
-                        req = helpers.removeParameter(req, parameter);
-
-                    }
-
-                    else if ("timestamp".equals(parameter.getName())) {
-                        data += parameter.getName()+"="+timestamp+"&";
-                        req = helpers.updateParameter(req, helpers.buildParameter("timestamp",timestamp,IParameter.PARAM_BODY));
-                    }
-
-                    else {
-                        data += parameter.getName()+"="+parameter.getValue()+"&";
-                    }
-                }
-            }
-
-            String Authorization = requestInfo.getHeaders().get(4).split(": ")[1];
-
-
-            //生成新签名
-            String apiKey = "apiKey=wxe0464377ab4efc7c";
-
-
-            String str3 = "";
-
-            stdout.println(data);
-            String[] arg = data.split("&");
-            Arrays.sort(arg);
-            for(String a:arg){
-
-                str3+=a+"&";
-            }
-            str3 = str3.substring(0,str3.length()-1);
-
-            String finalStr = apiKey+"&"+str3+Authorization;
-            stdout.println(finalStr);
-
-            sig = MD5Util.getMD5Str(finalStr);
-            stdout.println("sig: "+sig);
-            messageInfo.setRequest(helpers.addParameter(req,helpers.buildParameter("sig",sig, IParameter.PARAM_BODY)));
         }
 
     }
